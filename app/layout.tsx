03b58be812c9f7492727496ff321ea37f821c270
Aaron Junot <aaron@junot.codes>
2025-07-20 21:52:06 -0500

    feat(og-images): add logo to expansion

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Cooperative Codebase",
   description: "Landing page for cooperativecodebase.com",
+  openGraph: {
+    type: "website",
+    url: "https://cooperativecodebase.com",
+    title: "Cooperative Codebase",
+    description: "Landing page for cooperativecodebase.com",
+    siteName: "Cooperative Codebase",
+    images: [{ url: "https://cooperativecodebase.com/images/logo.png" }],
+  },
   generator: "v0.dev",
 };
 
